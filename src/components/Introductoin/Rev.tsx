f28b4ba71baf35000e70948d635e164479e60b24
alaliyo <des321321@naver.com>
2023-04-21 17:41:58 +0900

    feat : 담임 목사님 이미지 추가

--- src/components/Introductoin/Rev.tsx
+++ src/components/Introductoin/Rev.tsx
@@ -1,10 +1,22 @@
-import { Body, Title, HrBottom } from './IntroStyled';
+import { Card } from 'react-bootstrap';
+import { Body, Title, HrBottom, CardBox, CardFrame, CardText } from './IntroStyled';
+import RevImg from '../../imgs/RevImg.jpg';
 
 function Rev() {
     return(
         <Body>
             <Title>담임 목사</Title>
             <HrBottom />
+            <CardBox>
+                <CardFrame>
+                    <Card style={{ width: '10rem' }}>
+                        <Card.Img variant="top" src={RevImg} />
+                        <Card.Body>
+                            <CardText>홍경희 목사</CardText>
+                        </Card.Body>
+                    </Card>
+                </CardFrame>
+            </CardBox>
         </Body>
     );
 }
