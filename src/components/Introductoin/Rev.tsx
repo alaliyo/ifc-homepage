5587300134be337a08237b2fea99d787cec8d767
alaliyo <des321321@naver.com>
2023-04-22 22:43:51 +0900

    feat : 담임 목사님 약력 추가

--- src/components/Introductoin/Rev.tsx
+++ src/components/Introductoin/Rev.tsx
@@ -1,6 +1,7 @@
 import { Card } from 'react-bootstrap';
-import { Body, Title, HrBottom, CardBox, CardFrame, CardText } from './IntroStyled';
+import { Body, Title, HrBottom, CardBox, CardFrame, CardText, SeparationText } from './IntroStyled';
 import RevImg from '../../imgs/RevImg.jpg';
+import styled from 'styled-components';
 
 function Rev() {
     return(
@@ -17,8 +18,32 @@ function Rev() {
                     </Card>
                 </CardFrame>
             </CardBox>
+            <BriefHistoryBox>
+                <SeparationText>약력</SeparationText>
+                <ul>
+                    <li>JWM(Jesus World Mission) 스리랑카 신학교 교수</li>
+                    <li>아솔신학대학 교수</li>
+                    <li>VOCA 브니엘미스바 총회 회장</li>
+                    <li>브니엘 신학대학 이사</li>
+                    <li>열방교회 담임</li>
+                    <li>브니엘 신학대학원 졸업</li>
+                </ul>
+            </BriefHistoryBox>
         </Body>
     );
 }
 
 export default Rev;
+
+const BriefHistoryBox = styled.div`
+    padding: 0 30px;
+    margin-bottom: 50px;
+    @media screen and (max-width: 500px) {
+        padding: 0 10px;
+    }
+    li {
+        font-weight: 900;
+        margin-top: 10px;
+        word-break: keep-all;
+    }
+`;
