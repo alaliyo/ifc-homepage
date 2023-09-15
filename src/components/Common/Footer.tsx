dcfc747cd49c78f8364135208f8b231399e56756
alaliyo <des321321@naver.com>
2023-09-15 18:26:54 +0900

    feat: footer 밴드 링크 추가

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -3,18 +3,17 @@ import styled from "styled-components";
 function Footer() {
     return(
         <FooterBox>
-            <FooterTextBox>
-                <p>교회명: 열방교회</p>
-                <p>담임목사: 홍경희 목사</p>
-                <p>전화번호: 055-386-1080</p>
-                <p>주소: 경남 양산시 삼호동부6길 18</p>
-            </FooterTextBox>
+            <div>
+                <Text>교회명: 열방교회</Text>
+                <Text>담임목사: 홍경희 목사</Text>
+                <Text>전화번호: 055-386-1080</Text>
+            </div>
 
-            <FooterTextBox>
-                <p>이메일: fellowshipic2010@gmail.com</p>
-                <p>CCLI License #733821</p>
-                <p>CCLI Streaming License #2301412</p>
-            </FooterTextBox>
+            <div>
+                <Text>이메일: fellowshipic2010@gmail.com</Text>
+                <Text>주소: 경남 양산시 삼호동부6길 18</Text>
+                <Text as="a" href="https://band.us/band/48150076" target="_blank">밴드 링크</Text>
+            </div>
         </FooterBox>
     );
 }
@@ -27,7 +26,7 @@ const FooterBox = styled.footer`
     background-color: #8a8a8a;
     padding: 20px;
     display: grid;
-    grid-template-columns: 5fr 5fr;
+    grid-template-columns: 4fr 5fr;
 
     @media screen and (max-width: 650px) {
         padding: 15px;
@@ -42,24 +41,28 @@ const FooterBox = styled.footer`
     }
 `;
 
-const FooterTextBox = styled.div`
-    p {
+const Text = styled.p`
+    color: white;
+    font-weight: 900;
+    margin-bottom: 5px;
+    text-decoration: none;
+
+    :hover {
         color: white;
-        font-weight: 900;
-        margin-bottom: 5px;
-
-        @media screen and (max-width: 650px) {
-            font-size: 15px;
-            margin-bottom: 3px;
-        }
-
-        @media screen and (max-width: 560px) {
-            font-size: 12px;
-            margin-bottom: 2px;
-        }
-
-        @media screen and (max-width: 440px) {
-            font-size: 10px;
-        }
+        text-decoration: underline;
     }
-`;
+
+    @media screen and (max-width: 650px) {
+        font-size: 15px;
+        margin-bottom: 3px;
+    }
+
+    @media screen and (max-width: 560px) {
+        font-size: 12px;
+        margin-bottom: 2px;
+    }
+
+    @media screen and (max-width: 440px) {
+        font-size: 10px;
+    }
+`
